072bfa1481f6e6af9e56b096994c9eee2ef5b718
jessieost <jessie.osterman@gmail.com>
2015-06-16 15:28:43 +0200

    finished full name greeting

---
+++ JessieOsterman-jessieost/d2/full_name_greeting.rb
@@ -0,0 +1,14 @@
+puts "Hey what's your first name?"
+
+first_name = gets.chomp
+
+puts "Cool! What's your middle name?"
+
+middle_name = gets.chomp
+
+puts "Awesome, what about your last name?"
+
+last_name = gets.chomp
+
+puts "Wow, " + first_name + " " + middle_name + " " + last_name + " is such a cool name!"
+
