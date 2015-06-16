b7bc552fbb9e7f5da9d8965aedcf5b66efe9727e
jessieost <jessie.osterman@gmail.com>
2015-06-16 16:17:25 +0200

    bottles program finished

---
+++ JessieOsterman-jessieost/d2/99_bottles_of_beer.rb
@@ -0,0 +1,11 @@
+numberstart = 99
+number = numberstart
+
+while number > 2
+	puts number.to_s + "bottles of beer on the wall, "+ number.to_s+ " bottles of beer!"
+number = number - 1 
+	puts "Take one down, pass it around, "+ number.to_s + " bottles of beer on the wall!"
+end
+
+puts "2 bottles of beer on the wall, 2 bottles of beer! Take one down pass it around, 1 bottle of beer on the wall!"
+puts "1 bottle of beer on the wall, 1 bottle of beer! Take one down pass it around, no more bottles of beer on the wall!"
